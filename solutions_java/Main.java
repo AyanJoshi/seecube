ee11a5df940b78fe5de224dbc98442023053a025
Ayan Joshi <ajoshi27@asu.edu>
2020-10-01 14:17:26 -0700

    added input functionality

--- solutions_java/Main.java
+++ solutions_java/Main.java
@@ -2,6 +2,8 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Scanner scan = new Scanner(System.in);
+		String input = scan.next();
+		System.out.println("Hello Worldscsdc");
 	}
 }
